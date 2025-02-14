95653d42062f49b22bb4dc6e31b34e8dc20775ad
Igorodri <igorrmascarenhas@gmail.com>
2025-02-13 23:31:43 -0300

    Implementação de Usuários Back-End

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, jsonify, request, send_from_directory
+from flask import Flask, render_template, jsonify, request, send_from_directory, session
+from flask_session import Session
 from flask_cors import CORS
 import psycopg2
 
 app = Flask(__name__, static_folder="../Notas-Frontend/static", template_folder="../Notas-Frontend")
 
-CORS(app);
+app.config['SECRET_KEY'] = 'testenotas'  
+app.config['SESSION_TYPE'] = 'filesystem'  
+app.config['SESSION_PERMANENT'] = False  
+app.config['SESSION_USE_SIGNER'] = True  
+
+Session(app);
 
 DATABASE_URL = "postgresql://neondb_owner:npg_HA3pm7QItTkd@ep-aged-cell-a5ztp2ol.us-east-2.aws.neon.tech/neondb?sslmode=require"
 
-
 conexao = psycopg2.connect(DATABASE_URL)
+
+CORS(app);
 
 
 print("Conexão com o banco de dados realizada com sucesso!")
@@ -24,13 +31,17 @@ def page():
 
 @app.route('/notas')
 def select():
-    cursor = None;
     try:
+        if 'id_usuario' not in session:
+            return jsonify({'erro': 'Usuário não autenticado'}), 401
+        
+        id_usuario = session['id_usuario']
+
         cursor = conexao.cursor()
-        cursor.execute('SELECT * FROM "NOTAS_GERAIS"')
+        cursor.execute('SELECT * FROM "NOTAS_GERAIS" WHERE id_usuario = %s', (id_usuario,))
         notas = cursor.fetchall()
         
-        lista_notas = [{"id": nota[0], "titulo": nota[1], "descricao": nota[2], "data": nota[3]} for nota in notas]
+        lista_notas = [{"id": nota[0], "titulo": nota[1], "descricao": nota[2], "data": nota[3], "id_usuario": nota[4]} for nota in notas]
 
         return jsonify(lista_notas)
 
@@ -39,22 +50,27 @@ def select():
         return jsonify({"erro": str(e)}), 500
 
     finally:
-        if(cursor):
             cursor.close() 
 
 @app.route('/registrar', methods=['POST'])
 def registrar():
     try:
+        print(session)
+
+        if 'id_usuario' not in session:
+            return jsonify({'erro': 'Usuário não autenticado'}), 401
+
         registro = request.get_json()
         titulo = registro.get('titulo')
         descricao = registro.get('descricao')
+        id_usuario = session['id_usuario']
 
         if not titulo or not descricao:
             return jsonify({'erro': 'Título e descrição são obrigatórios'}), 400
 
         cursor = conexao.cursor()
 
-        cursor.execute('INSERT INTO "NOTAS_GERAIS" (titulo, descricao, data_criacao) VALUES (%s, %s, NOW())', (titulo, descricao))
+        cursor.execute('INSERT INTO "NOTAS_GERAIS" (titulo, descricao, data_criacao, id_usuario) VALUES (%s, %s, NOW(), %s)', (titulo, descricao, id_usuario))
         conexao.commit()
 
         return jsonify({'mensagem': 'Dados registrados com sucesso'}), 201
@@ -91,7 +107,6 @@ def excluir_dados():
 
 @app.route('/excluir_all', methods=['DELETE'])
 def excluir_all():
-    cursor = None;
     try:
         cursor = conexao.cursor()
 
@@ -104,12 +119,10 @@ def excluir_all():
         return jsonify({'erro': str(e)})
 
     finally:
-        if(cursor):
             cursor.close() 
 
 @app.route('/editar', methods=['PUT'])
 def editar_dados():
-    cursor = None;
     try:
         request_data = request.get_json()
         edit_id = request_data.get('id')
@@ -129,12 +142,10 @@ def editar_dados():
     except Exception as e:
         return jsonify({'erro': str(e)}), 500
     finally:
-        if(cursor):
             cursor.close() 
 
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar_usuario():
-    cursor = None;
     try:
         request_data = request.get_json();
         nome_usuario = request_data.get('nome_usuario');
@@ -154,12 +165,10 @@ def cadastrar_usuario():
         conexao.rollback();
         return jsonify ({'erro': str(e)}),500
     finally:
-        if(cursor):
             cursor.close() 
 
 @app.route('/autenticar', methods=['POST'])
 def buscar_usuario():
-    cursor = None 
     try:
         request_data = request.get_json()
         nome_usuario = request_data.get('nome_usuario')
@@ -174,6 +183,7 @@ def buscar_usuario():
         usuario = cursor.fetchone()
 
         if usuario:
+            session['id_usuario'] = usuario[0];
             return jsonify({'mensagem': 'Login bem-sucedido!', 'id_usuario': usuario[0]}), 200
         else:
             return jsonify({'erro': 'Usuário ou senha incorretos'}), 401
@@ -182,7 +192,6 @@ def buscar_usuario():
         return jsonify({'erro': str(e)}), 500
 
     finally:
-        if cursor:
             cursor.close()  
 
 
