c3aa2e03aece650a81d3973e8f6c37997fe62efd
Igorodri <igorrmascarenhas@gmail.com>
2025-05-11 00:17:13 -0300

    Alteração de Sessão

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, jsonify, request, send_from_directory, session
+from flask import Flask, jsonify, request, redirect,session
 from flask_session import Session
 from flask_cors import CORS
 import psycopg2
 
-app = Flask(__name__, static_folder="../Notas-Frontend/static", template_folder="../Notas-Frontend")
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'testenotas'  
 app.config['SESSION_TYPE'] = 'filesystem'  
@@ -23,63 +23,52 @@ print("Conexão com o banco de dados realizada com sucesso!")
 
 @app.route('/')
 def home():
-    return send_from_directory(app.template_folder, "index.html")
+    return redirect('http://127.0.0.1:5500')
 
-@app.route('/login')
-def page():
-    return send_from_directory(app.template_folder, "notas.html")
 
 @app.route('/notas')
 def select():
+    conn = None
+    cursor = None
     try:
-        if 'id_usuario' not in session:
-            return jsonify({'erro': 'Usuário não autenticado'}), 401
-        
-        id_usuario = session['id_usuario']
 
-        cursor = conexao.cursor()
-        cursor.execute('SELECT * FROM "NOTAS_GERAIS" WHERE id_usuario = %s', (id_usuario,))
+        conn = conexao
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM "NOTAS_GERAIS"')
         notas = cursor.fetchall()
         
-        lista_notas = [{"id": nota[0], "titulo": nota[1], "descricao": nota[2], "data": nota[3], "id_usuario": nota[4]} for nota in notas]
+        lista_notas = [{"id": nota[0], "titulo": nota[1], "descricao": nota[2], "data": nota[3]} for nota in notas]
 
         return jsonify(lista_notas)
 
     except Exception as e:
-        conexao.rollback()
         return jsonify({"erro": str(e)}), 500
 
-    finally:
-            cursor.close() 
 
 @app.route('/registrar', methods=['POST'])
 def registrar():
+    conn = None
+    cursor = None
     try:
         print(session)
-
-        if 'id_usuario' not in session:
-            return jsonify({'erro': 'Usuário não autenticado'}), 401
 
         registro = request.get_json()
         titulo = registro.get('titulo')
         descricao = registro.get('descricao')
-        id_usuario = session['id_usuario']
 
         if not titulo or not descricao:
             return jsonify({'erro': 'Título e descrição são obrigatórios'}), 400
 
-        cursor = conexao.cursor()
+        conn = conexao;
+        cursor = conn.cursor()
 
-        cursor.execute('INSERT INTO "NOTAS_GERAIS" (titulo, descricao, data_criacao, id_usuario) VALUES (%s, %s, NOW(), %s)', (titulo, descricao, id_usuario))
+        cursor.execute('INSERT INTO "NOTAS_GERAIS" (titulo, descricao, data_criacao) VALUES (%s, %s, NOW())', (titulo, descricao))
         conexao.commit()
 
         return jsonify({'mensagem': 'Dados registrados com sucesso'}), 201
 
     except Exception as e:
         return jsonify({'erro': str(e)}), 500
-
-    finally:
-            cursor.close() 
 
 @app.route('/excluir', methods=['DELETE'])
 def excluir_dados():
@@ -143,56 +132,6 @@ def editar_dados():
         return jsonify({'erro': str(e)}), 500
     finally:
             cursor.close() 
-
-@app.route('/cadastrar', methods=['POST'])
-def cadastrar_usuario():
-    try:
-        request_data = request.get_json();
-        nome_usuario = request_data.get('nome_usuario');
-        senha_usuario = request_data.get('senha_usuario');
-
-        if not nome_usuario or not senha_usuario:
-            return jsonify ({'erro': 'Nome e senha de usuário obrigatórios.'})
-
-        cursor = conexao.cursor()
-
-        cursor.execute('INSERT INTO "USUARIOS" (nome_usuario, senha_usuario) VALUES (%s, %s)', (nome_usuario, senha_usuario))
-        conexao.commit();
-
-        return jsonify({'mensagem': 'Usuário criado com sucesso!'}), 201
-    
-    except Exception as e:
-        conexao.rollback();
-        return jsonify ({'erro': str(e)}),500
-    finally:
-            cursor.close() 
-
-@app.route('/autenticar', methods=['POST'])
-def buscar_usuario():
-    try:
-        request_data = request.get_json()
-        nome_usuario = request_data.get('nome_usuario')
-        senha_usuario = request_data.get('senha_usuario')
-
-        if not nome_usuario or not senha_usuario:
-            return jsonify({'erro': 'Nome e senha são obrigatórios'}), 400
-
-        cursor = conexao.cursor()
-        cursor.execute('SELECT id FROM "USUARIOS" WHERE nome_usuario = %s AND senha_usuario = %s', 
-                       (nome_usuario, senha_usuario))
-        usuario = cursor.fetchone()
-
-        if usuario:
-            session['id_usuario'] = usuario[0];
-            return jsonify({'mensagem': 'Login bem-sucedido!', 'id_usuario': usuario[0]}), 200
-        else:
-            return jsonify({'erro': 'Usuário ou senha incorretos'}), 401
-
-    except Exception as e:
-        return jsonify({'erro': str(e)}), 500
-
-    finally:
-            cursor.close()  
 
 
 @app.route('/favicon.ico')
